6b45a0f0331823e4aa30fdff309a6321aa932096
tsheaff <ty.sheaffer@gmail.com>
2019-05-19 20:55:52 -0700

    initial commit with Xcode project

---
+++ ostisense/ostisense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ostisense
+//
+//  Created by Tyler Sheaffer on 5/19/19.
+//  Copyright © 2019 OstiSense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
